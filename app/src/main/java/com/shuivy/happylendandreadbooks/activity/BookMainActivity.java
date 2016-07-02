8bf3d9912476c1b03a431b1751f15a8643c39dee
Han shenquan <1173956412@qq.com>
2016-07-02 13:11:00 +0800

    互悦书吧第一版初始化完成

---
+++ app/src/main/java/com/shuivy/happylendandreadbooks/activity/BookMainActivity.java
@@ -0,0 +1,15 @@
+package com.shuivy.happylendandreadbooks.activity;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.shuivy.happylendandreadbooks.R;
+
+public class BookMainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.book_main);
+    }
+}
